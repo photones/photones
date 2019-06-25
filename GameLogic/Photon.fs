e2734cd30a21d21138cbc30847f29904d32a67fd
Chiel ten Brinke <ctenbrinke@gmail.com>
2019-06-25 09:45:23 +0200

    Fix naming to align with conventions

--- GameLogic/Photon.fs
+++ GameLogic/Photon.fs
@@ -33,33 +33,32 @@ type Photon(initialAttr: PhotonAttributes) =
     member public this.PoaIndex = attributes.PoaIndex
     member public this.Speed = attributes.Speed
 
-    static member pointsOfAttraction = [
+    static member PointsOfAttraction = [
         Position2(-0.1f,0.3f);
         Position2(0.6f,0.1f);
         Position2(0.0f,-0.6f);
         Position2(-0.3f,0.6f);
         ]
 
-    member private this.pointOfAttraction : Position2 = Photon.pointsOfAttraction.[this.PoaIndex]
-    member private this.hasReachedPointOfAttraction = Unit.op_LessThan((this.Position - this.pointOfAttraction).Length, Unit(attractionRadius))
+    member private this.PointOfAttraction : Position2 = Photon.PointsOfAttraction.[this.PoaIndex]
+    member private this.HasReachedPointOfAttraction = Unit.op_LessThan((this.Position - this.PointOfAttraction).Length, Unit(attractionRadius))
 
-    member private this.velocityToGoal (elapsedTime: TimeSpan) =
-        let diff = this.pointOfAttraction - this.Position
+    member private this.VelocityToGoal (elapsedTime: TimeSpan) =
+        let diff = this.PointOfAttraction - this.Position
         let acceleration = if diff.Length = Unit.Zero then new Acceleration2(0.0f, 0.0f) else new Acceleration2(diff.NumericValue.Normalized() * 3.0f)
         acceleration * elapsedTime
         
 
     member public this.Update (elapsedTime: TimeSpan) =
-        let vToGoal = this.velocityToGoal elapsedTime
+        let vToGoal = this.VelocityToGoal elapsedTime
         let velocity =
             this.Speed
             + capAccToGoal vToGoal
             + (smallRandomVelocity ())
             |> capTotal
         let position = this.Position + velocity * elapsedTime
-        let pointOfAttractionIndex = if this.hasReachedPointOfAttraction then (this.PoaIndex + 1) % Photon.pointsOfAttraction.Length else this.PoaIndex
+        let pointOfAttractionIndex = if this.HasReachedPointOfAttraction then (this.PoaIndex + 1) % Photon.PointsOfAttraction.Length else this.PoaIndex
         futureAttributes <- {Position = position; Speed = velocity; PoaIndex = pointOfAttractionIndex}
-        ()
 
     member public this.Refresh () =
         attributes <- futureAttributes
