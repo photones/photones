abf239a25d34e3767c19e622eb812a9c70707e6c
Chiel ten Brinke <ctenbrinke@gmail.com>
2019-09-17 17:12:24 +0200

    fix line checking

--- codecheck.sh
+++ codecheck.sh
@@ -21,7 +21,7 @@ fi
 # To replace all tabs: (warning! this introduces lf endings)
 #echo "$files_with_tabs" | xargs sed -zi 's/\t/    /g'
 
-# Check if all lines in source files end with a windows line ending
+# Check if all lines in source files end with a unix line ending
 files_with_crlf=$(git ls-files *.fs *.cs | xargs grep -UPlza '\r\n')
 if [[ -n "$files_with_crlf" ]]; then
     echo
@@ -35,13 +35,13 @@ fi
 #echo "$files_with_crlf" | xargs dos2unix
 
 # Check if the length of all source lines are limited by some predefined constant.
-too_long_lines=$(git grep -n '.\{100\}' -- *.fs *.cs)
+too_long_lines=$(git grep -Pn '(?<=.{100}).+' -- *.fs *.cs)
 if [[ -n "$too_long_lines" ]]; then
     echo
     echo "The following lines are too long:"
     echo
     # Repeat command for colored output
-    git grep -n '.\{100\}' -- *.fs *.cs
+    git grep -Pn '(?<=.{100}).+' -- *.fs *.cs
     exitcode=1
 fi
 
