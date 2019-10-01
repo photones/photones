99d9ed2b5666f898c6a4a24e6d8141c762868677
Chiel ten Brinke <ctenbrinke@gmail.com>
2019-10-01 10:12:34 +0200

    Add F# test project

---
+++ GameLogic.Test/Tests.fs
@@ -0,0 +1,15 @@
+﻿module Tests
+
+open System
+open Xunit
+
+let takeAtMost1 n (sequence:seq<'T>) =
+    let mutable i = n
+    Seq.takeWhile (fun _ ->
+        i <- i - 1
+        i >= 0
+        ) sequence |> Seq.toList
+
+[<Fact>]
+let ``My test`` () =
+    Assert.True(true)
