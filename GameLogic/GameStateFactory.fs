0155e10b6074e80ae4a02687cb8f3adfb40350aa
Chiel ten Brinke <ctenbrinke@gmail.com>
2019-10-11 10:42:04 +0200

    More players for more fun

--- GameLogic/GameStateFactory.fs
+++ GameLogic/GameStateFactory.fs
@@ -8,17 +8,33 @@ module GameStateFactory =
     let BuildInitialGameState() =
         let player1 = Player(0uy, Color.Goldenrod) :> IUpdatetablePlayer<GameState>
         let player2 = Player(1uy, Color.HotPink) :> IUpdatetablePlayer<GameState>
+        let player3 = Player(2uy, Color.Green) :> IUpdatetablePlayer<GameState>
+        let player4 = Player(3uy, Color.Red) :> IUpdatetablePlayer<GameState>
         let planet1 = Planet.CreatePlanet ({
             Position = Position2(-1.0f, 0.0f);
-            Size = Unit 0.1f;
+            Size = Unit 0.05f;
             Alive = true;
             Player = player1;
         })
         let planet2 = Planet.CreatePlanet ({
             Position = Position2(1.0f, 0.0f);
-            Size = Unit 0.1f;
+            Size = Unit 0.05f;
             Alive = true;
             Player = player2;
         })
-        GameState(List<IUpdatetablePlayer<GameState>>([player1;player2]), List<GameObject<GameState>>([planet1;planet2]))
+        let planet3 = Planet.CreatePlanet ({
+            Position = Position2(0.0f, 0.8f);
+            Size = Unit 0.05f;
+            Alive = true;
+            Player = player3;
+        })
+        let planet4 = Planet.CreatePlanet ({
+            Position = Position2(0.0f, -0.8f);
+            Size = Unit 0.05f;
+            Alive = true;
+            Player = player4;
+        })
+        let players = List<IUpdatetablePlayer<GameState>>([player1;player2;player3;player4])
+        let planets = List<GameObject<GameState>>([planet1;planet2;planet3;planet4])
+        GameState(players, planets)
 
